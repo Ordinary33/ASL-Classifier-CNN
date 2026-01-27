5f6772010b4d470eadcea97aaff731a5ca63b530
Ordinary33 <husnie.baddie@g.msuiit.edu.ph>
2026-01-28 01:23:38 +0800

    feat: Add inference averaging to improve model prediction stability.

--- src/run_webcam.py
+++ src/run_webcam.py
@@ -1,4 +1,6 @@
 import cv2 as cv
+import statistics
+from collections import deque
 from src.predict import ASLClassifier
 
 
@@ -8,6 +10,8 @@ def main():
     except Exception as e:
         print(e)
         return
+
+    prediction_history = deque(maxlen=5)
 
     cap = cv.VideoCapture(0)
     if not cap.isOpened():
@@ -31,8 +35,20 @@ def main():
         if roi_crop.size != 0:
             label, confidence = model.classify_frame(roi_crop)
 
-            color = (0, 255, 0) if confidence > 0.7 else (0, 0, 255)
-            text = f"label: {label}, conf: {confidence:.2f}"
+            prediction_history.append(label)
+
+            try:
+                stable_level = statistics.mode(prediction_history)
+
+            except statistics.StatisticsError:
+                stable_level = label
+
+            color = (0, 255, 0) if confidence > 0.75 else (0, 0, 255)
+
+            if confidence > 0.5:
+                text = f"label: {stable_level}, conf: {confidence:.2f}"
+            else:
+                text = "..."
 
             cv.putText(frame, text, (x1, y1 - 10), cv.FONT_HERSHEY_SIMPLEX, 1, color, 2)
 
