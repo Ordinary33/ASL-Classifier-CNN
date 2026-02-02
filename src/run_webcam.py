203b3ae3f3377d979c3e99f036dfb91e9ff31b7d
Ordinary33 <husnie.baddie@g.msuiit.edu.ph>
2026-02-02 23:24:00 +0800

    feat: Add Mediapipe handtracking.

--- src/run_webcam.py
+++ src/run_webcam.py
@@ -1,7 +1,11 @@
 import cv2 as cv
+import mediapipe as mp
 import statistics
 from collections import deque
 from src.predict import ASLClassifier
+
+mp_hands = mp.solutions.hands
+mp_draw = mp.solutions.drawing_utils
 
 
 def main():
@@ -18,44 +22,60 @@ def main():
         print("Cannot open camera")
         exit()
 
-    while True:
-        ret, frame = cap.read()
+    with mp_hands.Hands(
+        max_num_hands=1,
+        min_detection_confidence=0.7,
+        min_tracking_confidence=0.7,
+    ) as hands:
+        while True:
+            ret, frame = cap.read()
 
-        if not ret:
-            print("Cannot receive frame (stream end?). Exiting ...")
-            break
+            if not ret:
+                print("Cannot receive frame (stream end?). Exiting ...")
+                break
 
-        frame = cv.flip(frame, 1)
+            frame = cv.flip(frame, 1)
+            h, w, _ = frame.shape
+            rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+            result = hands.process(rgb)
 
-        x1, y1, x2, y2 = 350, 50, 600, 300
-        cv.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            if result.multi_hand_landmarks:
+                for hand_landmarks in result.multi_hand_landmarks:
+                    mp_draw.draw_landmarks(
+                        frame, hand_landmarks, mp_hands.HAND_CONNECTIONS
+                    )
 
-        roi_crop = frame[y1:y2, x1:x2]
+            x1, y1, x2, y2 = 350, 50, 600, 300
+            cv.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-        if roi_crop.size != 0:
-            label, confidence = model.classify_frame(roi_crop)
+            roi_crop = frame[y1:y2, x1:x2]
 
-            prediction_history.append(label)
+            if roi_crop.size != 0:
+                label, confidence = model.classify_frame(roi_crop)
 
-            try:
-                stable_level = statistics.mode(prediction_history)
+                prediction_history.append(label)
 
-            except statistics.StatisticsError:
-                stable_level = label
+                try:
+                    stable_level = statistics.mode(prediction_history)
 
-            color = (0, 255, 0) if confidence > 0.75 else (0, 0, 255)
+                except statistics.StatisticsError:
+                    stable_level = label
 
-            if confidence > 0.5:
-                text = f"label: {stable_level}, conf: {confidence:.2f}"
-            else:
-                text = "..."
+                color = (0, 255, 0) if confidence > 0.75 else (0, 0, 255)
 
-            cv.putText(frame, text, (x1, y1 - 10), cv.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+                if confidence > 0.5:
+                    text = f"label: {stable_level}, conf: {confidence:.2f}"
+                else:
+                    text = "..."
 
-        cv.imshow("ASL Translator", frame)
+                cv.putText(
+                    frame, text, (x1, y1 - 10), cv.FONT_HERSHEY_SIMPLEX, 1, color, 2
+                )
 
-        if cv.waitKey(1) & 0xFF == ord("q"):
-            break
+            cv.imshow("ASL Translator", frame)
+
+            if cv.waitKey(1) & 0xFF == ord("q"):
+                break
 
     cap.release()
     cv.destroyAllWindows()
