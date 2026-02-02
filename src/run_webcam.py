660e096dae7e7ec59384bf99c3b03f9b71fc19e7
Ordinary33 <husnie.baddie@g.msuiit.edu.ph>
2026-02-03 00:01:52 +0800

    feat: Add dynamic box around the hand being tracked.

--- src/run_webcam.py
+++ src/run_webcam.py
@@ -8,6 +8,48 @@ mp_hands = mp.solutions.hands
 mp_draw = mp.solutions.drawing_utils
 
 
+def get_bbox_coordinates(landmarks, image_shape):
+    h, w, c = image_shape
+    x_max, y_max = 0, 0
+    x_min, y_min = w, h
+
+    for lm in landmarks.landmark:
+        x, y = int(lm.x * w), int(lm.y * h)
+        if x > x_max:
+            x_max = x
+        if x < x_min:
+            x_min = x
+        if y > y_max:
+            y_max = y
+        if y < y_min:
+            y_min = y
+
+    offset = 40
+    y_min -= offset
+    y_max += offset
+    x_min -= offset
+    x_max += offset
+
+    box_w = x_max - x_min
+    box_h = y_max - y_min
+
+    if box_w > box_h:
+        diff = (box_w - box_h) // 2
+        y_min -= diff
+        y_max += diff
+    else:
+        diff = (box_h - box_w) // 2
+        x_min -= diff
+        x_max += diff
+
+    x_min = max(0, x_min)
+    y_min = max(0, y_min)
+    x_max = min(w, x_max)
+    y_max = min(h, y_max)
+
+    return x_min, y_min, x_max, y_max
+
+
 def main():
     try:
         model = ASLClassifier()
@@ -35,42 +77,48 @@ def main():
                 break
 
             frame = cv.flip(frame, 1)
-            h, w, _ = frame.shape
+            h, w, c = frame.shape
             rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
             result = hands.process(rgb)
 
             if result.multi_hand_landmarks:
                 for hand_landmarks in result.multi_hand_landmarks:
+                    x1, y1, x2, y2 = get_bbox_coordinates(hand_landmarks, (h, w, c))
+
+                    cv.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 2)
+                    roi_crop = frame[y1:y2, x1:x2]
+
+                    if roi_crop.size != 0:
+                        label, confidence = model.classify_frame(roi_crop)
+
+                        prediction_history.append(label)
+
+                        try:
+                            stable_level = statistics.mode(prediction_history)
+
+                        except statistics.StatisticsError:
+                            stable_level = label
+
+                        color = (0, 255, 0) if confidence > 0.75 else (0, 0, 255)
+
+                        if confidence > 0.5:
+                            text = f"label: {stable_level}, conf: {confidence:.2f}"
+                        else:
+                            text = "..."
+
+                        cv.putText(
+                            frame,
+                            text,
+                            (x1, y1 - 10),
+                            cv.FONT_HERSHEY_SIMPLEX,
+                            1,
+                            color,
+                            2,
+                        )
+
                     mp_draw.draw_landmarks(
                         frame, hand_landmarks, mp_hands.HAND_CONNECTIONS
                     )
-
-            x1, y1, x2, y2 = 350, 50, 600, 300
-            cv.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
-            roi_crop = frame[y1:y2, x1:x2]
-
-            if roi_crop.size != 0:
-                label, confidence = model.classify_frame(roi_crop)
-
-                prediction_history.append(label)
-
-                try:
-                    stable_level = statistics.mode(prediction_history)
-
-                except statistics.StatisticsError:
-                    stable_level = label
-
-                color = (0, 255, 0) if confidence > 0.75 else (0, 0, 255)
-
-                if confidence > 0.5:
-                    text = f"label: {stable_level}, conf: {confidence:.2f}"
-                else:
-                    text = "..."
-
-                cv.putText(
-                    frame, text, (x1, y1 - 10), cv.FONT_HERSHEY_SIMPLEX, 1, color, 2
-                )
 
             cv.imshow("ASL Translator", frame)
 
